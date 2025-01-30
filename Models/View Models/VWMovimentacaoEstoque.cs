ad10353cbf238a5370e38f2adb2c9a525b5fc2c3
ArthurAndrade1406 <arthur.andrade140606@gmail.com>
2025-01-30 15:13:49 -0300

    Demanda Silva Implementada

---
+++ Models/View Models/VWMovimentacaoEstoque.cs
@@ -0,0 +1,28 @@
+﻿namespace Projeto_Mercado_API.Models.View_Models
+{
+    public class VWMovimentacaoEstoque
+    {
+        public int idProduto { get; set; }
+        public int quantidade { get; set; }
+        public int idEstoqueOrigem { get; set; }
+        public int idEstoqueDestino { get; set; }
+        public int idFuncionarioSolicitador { get; set; }
+        public int idFuncionarioAutenticador { get; set; }
+        public int idTipoMovimentacaoOrigem { get; set; }
+        public int idTipoMovimentacaoDestino { get; set; }
+        public DateTime dataHora { get; set; }
+
+        public VWMovimentacaoEstoque()
+        {
+            idProduto = 0;
+            quantidade = 0;
+            idEstoqueOrigem = 0;
+            idEstoqueDestino = 0;
+            idFuncionarioSolicitador = 0;
+            idFuncionarioAutenticador = 0;
+            idTipoMovimentacaoOrigem = 0;
+            idTipoMovimentacaoDestino = 0;
+            dataHora = DateTime.Now;
+        }
+    }
+}
