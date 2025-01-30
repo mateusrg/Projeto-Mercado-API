d5c1b89f1b00192a3bed9ecc9be9e0c762eb541a
Miihts <milenatsilveira104@gmail.com>
2025-01-30 15:03:56 -0300

    Compra Completa add

---
+++ Models/View Models/CompraCompleta.cs
@@ -0,0 +1,27 @@
+﻿namespace Projeto_Mercado_API.Models.View_Models
+{
+    public class CompraCompleta
+    {
+        public int IdCompra { get; set; }
+        public int IdProduto { get; set; }
+        public string Descricao { get; set; }
+        public int Quantidade { get; set; }
+        public int IdFornecedor { get; set; }
+        public string Nome { get; set; }
+        public string CNPJ { get; set; }
+        public DateTime Data { get; set; }
+
+        public CompraCompleta()
+        {
+            IdCompra = 0;
+            IdFornecedor = 0;
+            IdProduto = 0;
+            Data = new DateTime();
+            Quantidade = 0;
+            Nome = "Sem nome";
+            CNPJ = "Sem CNPJ";
+            Descricao = "Sem descrição";
+        }
+    }
+
+}
