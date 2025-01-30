614998c4f8a85fee67ca6b1fb02b3cb1a29436a3
LeonardoAndrade77 <leo.andrade.eder@gmail.com>
2025-01-30 15:20:54 -0300

    login do léo

--- Controllers/FuncionariosController.cs
+++ Controllers/FuncionariosController.cs
@@ -14,62 +14,18 @@ namespace Projeto_Mercado_API.Controllers
             return Ok(FuncionariosRepository.ListarTodos());
         }
 
-        [HttpGet]
-        [Route("id/{idFuncionario}")]
-        public ActionResult<Funcionario?> ConsultarPorId(int idFuncionario)
-        {
-            var funcionario = FuncionariosRepository.ConsultarPorId(idFuncionario);
-            if (funcionario == null)
-                return BadRequest("Funcionário não encontrado.");
-            return Ok(funcionario);
-        }
-
-        [HttpGet]
-        [Route("nome/{nome}")]
-        public ActionResult<List<Funcionario>> ConsultarPorNome(string nome)
-        {
-            return Ok(FuncionariosRepository.ConsultarPorNome(nome));
-        }
-
-        [HttpGet]
-        [Route("setor/{setor}")]
-        public ActionResult<List<Funcionario>> ConsultarPorSetor(string setor)
-        {
-            return Ok(FuncionariosRepository.ConsultarPorNome(setor));
-        }
-
-        [HttpGet]
-        [Route("email/{email}")]
-        public ActionResult<Funcionario> ConsultarPorEmail(string email)
-        {
-            return Ok(FuncionariosRepository.ConsultarPorEmail(email));
-        }
-
         [HttpPost]
-        public ActionResult<int> Cadastrar(Funcionario novoFuncionario)
+        [Route("login")]
+        public ActionResult<Funcionario> Login([FromBody] PaginaLogin request)
         {
-            return Ok(FuncionariosRepository.Cadastrar(novoFuncionario));
-        }
+            var funcionario = FuncionariosRepository.ConsultarPorEmail(request.Email);
 
-        [HttpPut]
-        public ActionResult<int> Alterar(Funcionario funcionarioAlterar)
-        {
-            if (funcionarioAlterar.IdFuncionario <= 0)
+            if (funcionario == null || funcionario.Senha != request.Senha)
             {
-                return BadRequest("O ID do funcionário deve ser maior do que 0.");
+                return NotFound("E-mail ou senha inválidos.");
             }
-            return Ok(FuncionariosRepository.Alterar(funcionarioAlterar));
-        }
 
-        [HttpDelete]
-        [Route("{idFuncionario}")]
-        public ActionResult<int> ExcluirPorId(int idFuncionario)
-        {
-            if (idFuncionario <= 0)
-            {
-                return BadRequest("O ID do funcionário deve ser maior do que 0.");
-            }
-            return Ok(FuncionariosRepository.ExcluirPorId(idFuncionario));
+            return Ok(funcionario);
         }
     }
 }
