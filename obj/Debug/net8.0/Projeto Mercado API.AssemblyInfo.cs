ac516048134e514f4534086675ea5c7c4356f61c
LeonardoAndrade77 <leo.andrade.eder@gmail.com>
2025-02-18 15:07:05 -0300

    adicionado o "VALUES" na página TiposMovimentacaoEstoqueRepository

--- obj/Debug/net8.0/Projeto Mercado API.AssemblyInfo.cs
+++ obj/Debug/net8.0/Projeto Mercado API.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Projeto Mercado API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+20f247b393c108373b57fd8f467daf94c5d3a552")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+69e725c7fed640a897949db6ecff5ac6fc236eec")]
 [assembly: System.Reflection.AssemblyProductAttribute("Projeto Mercado API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Projeto Mercado API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
