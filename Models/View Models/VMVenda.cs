20f247b393c108373b57fd8f467daf94c5d3a552
Esta1te <nicolasestaite@gmail.com>
2025-02-18 13:58:40 -0300

    adição da vm venda

--- Models/View Models/VMVenda.cs
+++ Models/View Models/VMVenda.cs
@@ -2,9 +2,9 @@
 {
     public class VMVenda
     {
-        public int IdMovimentacaoEstoque { get; set; }
         public int IdFuncionarioSolicitador { get; set; }
         public int IdFuncionarioAutenticador { get; set; }
+        public int IdEstoque { get; set; }
         public int IdProduto { get; set; }
         public int Quantidade { get; set; }
         public DateTime DataHora { get; set; }
