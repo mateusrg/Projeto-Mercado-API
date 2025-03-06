8ef637b8c91a7db1770f7dfaf0c4717188b9f150
isabele30 <isabelefernandes191@gmail.com>
2025-03-06 14:55:50 -0300

    Mudanças na API

--- obj/Debug/net8.0/Projeto Mercado API.AssemblyInfo.cs
+++ obj/Debug/net8.0/Projeto Mercado API.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Projeto Mercado API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+69e725c7fed640a897949db6ecff5ac6fc236eec")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a17c08426ce0df98d8b215ac2af969ce155ab279")]
 [assembly: System.Reflection.AssemblyProductAttribute("Projeto Mercado API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Projeto Mercado API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
