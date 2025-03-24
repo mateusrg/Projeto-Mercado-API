39a0bd58fccb02b62846537510cf844341868073
mateusrg <mateusrgewehr@gmail.com>
2025-03-24 16:19:50 -0300

    Create VMEstoqueQuantidadeProduto.cs

---
+++ Models/View Models/VMEstoqueQuantidadeProduto.cs
@@ -0,0 +1,14 @@
+﻿namespace Projeto_Mercado_API.Models.View_Models
+{
+    public class VMEstoqueQuantidadeProduto
+    {
+        public string Estoque { get; set; }
+        public int Quantidade { get; set; }
+
+        public VMEstoqueQuantidadeProduto()
+        {
+            Estoque = "";
+            Quantidade = 0;
+        }
+    }
+}
